52b0ad42f364eaf3f28bc65cad3b8828647f9755
Pranit5895 <guptapranit9@gmail.com>
2020-05-05 09:31:17 -0700

    enqueue() function declared for enqueing queue

--- Queue/Queue-using-array.cpp
+++ Queue/Queue-using-array.cpp
@@ -8,4 +8,21 @@ class Queue {
  public:
   // Initialize queue of maximum size 'capacity'
   explicit Queue(size_t capacity) : entries_(capacity) {}
+// Enqueue function
+  void Enqueue(int x) {
+    if (tail_ == entries_.size()) {
+      // If number of elements in queue is equal to maximum capacity
+      if (num_queue_elements == entries_.size()) {
+        throw length_error("queue overflow");
+        return;
+      } else {
+        // If number of elements in queue less than maximum queue size,
+        // rearrange
+        rotate(entries_.begin(), entries_.begin() + head_, entries_.end());
+        head_ = 0, tail_ = num_queue_elements;
+      }
+    }
 
+    entries_[tail_] = x;
+    ++tail_, ++num_queue_elements;
+  }

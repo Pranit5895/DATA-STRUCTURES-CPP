89045a618c6a04716b29064b82a4b4f94a867773
Pranit5895 <guptapranit9@gmail.com>
2020-05-06 04:27:11 -0700

    Structure Queue completed

---
+++ Queue/Queue-using-linked-list.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+// A linked list (LL) node to store a queue entry
+struct QNode
+{
+    int key;
+    struct QNode *next;
+};
+
+// The queue, front stores the front node of LL and rear stores ths
+// last node of LL
+struct Queue
+{
+    struct QNode *front, *rear;
+};
+

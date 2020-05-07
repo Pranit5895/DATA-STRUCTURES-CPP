85a0e78377431833a01f9fb8f3007132b3a6858d
Pranit5895 <guptapranit9@gmail.com>
2020-05-07 10:29:29 -0700

    push() function declared for inserting in stack

--- Stack/Stack-using-arrays.cpp
+++ Stack/Stack-using-arrays.cpp
@@ -7,3 +7,10 @@ struct Stack
     int data;
     Stack *next;
 } *TOS = NULL;
+void Push(int value)
+{
+    Stack *new_node = new Stack();
+    new_node -> data = value;
+    new_node -> next = TOS;
+    TOS = new_node;
+}

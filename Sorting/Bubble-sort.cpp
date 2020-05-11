3ccaa1ffc38a00d252fa5974062fe84e4ae7bd36
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 04:58:12 -0700

    Function for bubble sort

--- Sorting/Bubble-sort.cpp
+++ Sorting/Bubble-sort.cpp
@@ -11,3 +11,11 @@ void display(int *array, int size) {
       cout << array[i] << " ";
    cout << endl;
 }
+void bubbleSort(int *array, int size) {
+   for(int i = 0; i<size; i++) {
+      int swaps = 0;
+      for(int j = 0; j<size-i-1; j++) {
+         if(array[j] > array[j+1]) {
+            swapping(array[j], array[j+1]);
+            swaps = 1;
+         }

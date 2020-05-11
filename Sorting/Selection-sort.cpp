bfdac8b2f6e423acb0884417916ebfcd902c6666
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 05:12:03 -0700

    Selection sort function added

--- Sorting/Selection-sort.cpp
+++ Sorting/Selection-sort.cpp
@@ -11,3 +11,14 @@ void display(int *array, int size) {
       cout << array[i] << " ";
    cout << endl;
 }
+void selectionSort(int *array, int size) {
+   int i, j, imin;
+   for(i = 0; i<size-1; i++) {
+      imin = i;   //get index of minimum data
+      for(j = i+1; j<size; j++)
+         if(array[j] < array[imin])
+            imin = j;
+         //placing in correct position
+         swap(array[i], array[imin]);
+   }
+}

6e00635b67b2c84db9ff704bc5fc7409948cc8dd
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 04:57:39 -0700

    Display function added

---
+++ Sorting/Bubble-sort.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void swapping(int &a, int &b) {
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
+}
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}

c007984bcab8a83eacbca12f2c5724dfda322aea
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 05:03:56 -0700

    Insertion sort function added

--- Sorting/Insertion-sort.cpp
+++ Sorting/Insertion-sort.cpp
@@ -5,3 +5,15 @@ void display(int *array, int size) {
       cout << array[i] << " ";
    cout << endl;
 }
+void insertionSort(int *array, int size) {
+   int key, j;
+   for(int i = 1; i<size; i++) {
+      key = array[i];//take value
+      j = i;
+      while(j > 0 && array[j-1]>key) {
+         array[j] = array[j-1];
+         j--;
+      }
+      array[j] = key;   //insert in right place
+   }
+}

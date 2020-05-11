5fdaeb66a8ffc7fcd121f22e317f4ba3ca20ae62
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 05:18:22 -0700

    Merging temp array into main array

--- Sorting/Merge-sort.cpp
+++ Sorting/Merge-sort.cpp
@@ -20,3 +20,23 @@ void merge(int arr[], int l, int m, int r)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1+ j];
+    /* Merge the temp arrays back into arr[l..r]*/
+
+    i = 0;  // Initial index of first subarray
+    j = 0;  // Initial index of second subarray
+    k = l;  // Initial index of merged subarray
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }

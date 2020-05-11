c31901fc038127dc6cc70be34ff570161c740c1a
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 05:23:55 -0700

    main() function started for function calling

--- Sorting/Merge-sort.cpp
+++ Sorting/Merge-sort.cpp
@@ -82,3 +82,12 @@ void printArray(int A[], int size)
         cout<<A[i]<<" ";
     cout<<"\n";
 }
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<"Given array is \n";
+    printArray(arr, arr_size);
+
+    mergeSort(arr, 0, arr_size - 1);

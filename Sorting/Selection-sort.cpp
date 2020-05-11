499a8d71aa7a39118a3e0608d4e909484f717ced
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 05:12:42 -0700

    main() function started for function calling

--- Sorting/Selection-sort.cpp
+++ Sorting/Selection-sort.cpp
@@ -22,3 +22,12 @@ void selectionSort(int *array, int size) {
          swap(array[i], array[imin]);
    }
 }
+int main() {
+   int n;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];           //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }

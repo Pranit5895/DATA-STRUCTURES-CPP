bf040ce21b70ade6aeb5fd44aba73ce2a6cddf6e
Pranit5895 <guptapranit9@gmail.com>
2020-05-11 05:16:52 -0700

    Function for merging two arrays started

--- Sorting/Merge-sort.cpp
+++ Sorting/Merge-sort.cpp
@@ -5,3 +5,12 @@ using namespace std;
 // Merges two subarrays of arr[]. 
 // First subarray is arr[l..m] 
 // Second subarray is arr[m+1..r] 
+/
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+
+    int L[n1], R[n2];  /* create temp arrays */

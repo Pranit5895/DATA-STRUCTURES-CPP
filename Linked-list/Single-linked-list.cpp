4541b7860e67af0e6f69c2a8bed7c86a11909b23
Pranit5895 <guptapranit9@gmail.com>
2020-06-03 04:09:18 -0700

    main() function started with menu declared

--- Linked-list/Single-linked-list.cpp
+++ Linked-list/Single-linked-list.cpp
@@ -31,3 +31,26 @@ class single_llist
             start = NULL;
         }
 };
+/*
+ * Main :contains menu
+ */
+main()
+{
+    int choice, nodes, element, position, i;
+    single_llist sl;
+    start = NULL;
+    while (1)
+    {
+        cout<<endl<<"---------------------------------"<<endl;
+        cout<<endl<<"Operations on singly linked list"<<endl;
+        cout<<endl<<"---------------------------------"<<endl;
+        cout<<"1.Insert Node at beginning"<<endl;
+        cout<<"2.Insert node at last"<<endl;
+        cout<<"3.Insert node at position"<<endl;
+        cout<<"4.Sort Link List"<<endl;
+        cout<<"5.Delete a Particular Node"<<endl;
+        cout<<"6.Update Node Value"<<endl;
+        cout<<"7.Search Element"<<endl;
+        cout<<"8.Display Linked List"<<endl;
+        cout<<"9.Reverse Linked List "<<endl;
+        cout<<"10.Exit "<<endl;

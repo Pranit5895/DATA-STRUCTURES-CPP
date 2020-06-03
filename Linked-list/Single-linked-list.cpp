693b6afa41100791bc47ffa372e1a6ad0d84ad2e
Pranit5895 <guptapranit9@gmail.com>
2020-06-03 04:08:33 -0700

    Class declaration

--- Linked-list/Single-linked-list.cpp
+++ Linked-list/Single-linked-list.cpp
@@ -10,3 +10,24 @@ struct node
     int info;
     struct node *next;
 }*start;
+/*
+ * Class Declaration
+ */
+class single_llist
+{
+    public:
+        node* create_node(int);
+        void insert_begin();
+        void insert_pos();
+        void insert_last();
+        void delete_pos();
+        void sort();
+        void search();
+        void update();
+        void reverse();
+        void display();
+        single_llist()
+        {
+            start = NULL;
+        }
+};

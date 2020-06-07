5a671a3969e05040f7179998836a16d2bce10b8a
Pranit5895 <guptapranit9@gmail.com>
2020-06-07 01:41:32 -0700

    Push() function declared for inserting values

--- Linked-list/Stack-using-linked-list.cpp
+++ Linked-list/Stack-using-linked-list.cpp
@@ -5,3 +5,9 @@ struct Node {
    struct Node *next;
 };
 struct Node* top = NULL;
+void push(int val) {
+   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node));
+   newnode->data = val;
+   newnode->next = top;
+   top = newnode;
+}

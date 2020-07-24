ec15c4b7893ee91e9e1daeb3492d39fe0ccda75b
Pranit5895 <guptapranit9@gmail.com>
2020-07-24 06:14:26 -0700

    Function to insert value in an array

--- Linked-list/Queue-using-Array.cpp
+++ Linked-list/Queue-using-Array.cpp
@@ -4,3 +4,26 @@
 
 int front = -1, rear = -1;
 int queue[maxsize];
+
+void insert(){
+
+	int item;
+	printf("\nEnter the item: ");
+	scanf("%d", &item);
+
+	if(front>rear || rear>maxsize){
+		printf("\nOVERFLOW");
+		return;
+	}
+	else if(front == -1 && rear == -1){
+		front = 0;
+		rear = 0;
+	}
+
+	else{
+		rear+=1;
+	}
+
+	queue[rear] = item;
+	printf("\nInserted");
+}
